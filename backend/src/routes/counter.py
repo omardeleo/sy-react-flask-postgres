c629e1472c8a1b86d233e80167fe82ab8de82ca6
Omar De Leo <omar.deleo@gmail.com>
2021-02-03 14:59:23 -0500

    Update flask backend to serve JSON response instead of html template

--- backend/src/routes/counter.py
+++ backend/src/routes/counter.py
@@ -1,9 +1,12 @@
 from flask import (
     Blueprint,
-    render_template
+    render_template,
+    jsonify
 )
 
 from src.models import Counter
+
+import datetime
 
 
 blueprint = Blueprint('counter', __name__)
@@ -13,4 +16,11 @@ blueprint = Blueprint('counter', __name__)
 def index():
     counter = Counter.get_create(label='Test')
     counter.increment()
-    return render_template('counter.html', counters=Counter.list())
+    date = datetime.datetime.now()
+    dateStr = date.strftime('%c')
+    response = f"""Hi! I\'m a Flask server.\n
+I\'m running on port 8080.
+I\'ve been pinged {counter.count} times.
+Most recent ping on {dateStr}."""
+
+    return jsonify(response=response)
