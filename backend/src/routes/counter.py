4ea933dd0989493ea4c12a2f9e1dbf9074c90c47
Omar De Leo <omar.deleo@gmail.com>
2021-02-03 14:45:16 -0500

    Add files from original SY Flask starter to test Flask works alone

--- backend/src/routes/counter.py
+++ backend/src/routes/counter.py
@@ -1,26 +1,16 @@
 from flask import (
     Blueprint,
-    render_template,
-    jsonify
+    render_template
 )
 
 from src.models import Counter
-
-import datetime
 
 
 blueprint = Blueprint('counter', __name__)
 
 
-@blueprint.route('/api/v1/')
+@blueprint.route('/')
 def index():
     counter = Counter.get_create(label='Test')
     counter.increment()
-    date = datetime.datetime.now()
-    dateStr = date.strftime('%c')
-    response = f"""Hi! I\'m a Flask server.\n
-I\'m running on port 8080.
-I\'ve been pinged {counter.count} times.
-Most recent ping on {dateStr}."""
-
-    return jsonify(response=response)
+    return render_template('counter.html', counters=Counter.list())
