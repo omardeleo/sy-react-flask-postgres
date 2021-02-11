62b936d6a2fb623e837c171027c54ed5b6a5fd35
Omar De Leo <omar.deleo@gmail.com>
2021-02-10 21:35:43 -0500

    Add api/v1 namespace to Localstack routes

--- backend/src/routes/localstack.py
+++ backend/src/routes/localstack.py
@@ -16,13 +16,13 @@ def s3_client():
     return localstack_client.session.Session().client('s3')
 
 
-@blueprint.route('/files/')
+@blueprint.route('/api/v1/files/')
 def files():
     s3_client().create_bucket(Bucket=BUCKET)
     return jsonify(s3_client().list_objects(Bucket=BUCKET))
 
 
-@blueprint.route('/files/create/')
+@blueprint.route('/api/v1/files/create/')
 def add_file():
     s3_client().put_object(Bucket=BUCKET,
                            Key='{}.txt'.format(uuid.uuid1()),
